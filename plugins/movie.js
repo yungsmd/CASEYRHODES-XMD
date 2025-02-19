545b11c5d1db1a2960d3f0c17dfeb1b9f2b059b8
caseyweb <caseywebsthedonn25@gmail.com>
2025-02-19 15:20:25 +0300

    ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊🫅

--- plugins/movie.js
+++ plugins/movie.js
@@ -25,7 +25,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         }
 
         const movieInfo = `
-🎬 *Silva Spark Movie Information* 🎬
+🎬 *ᴄᴀsᴇʏʀʜᴏᴅᴇs Spark Movie Information* 🎬
 
 🎥 *Title:* ${data.Title}
 📅 *Year:* ${data.Year}
@@ -50,7 +50,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         // Send the movie information along with the poster image
         await conn.sendMessage(from, {
             image: { url: imageUrl },
-            caption: `${movieInfo}\n> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX`
+            caption: `${movieInfo}\n> ©ᴄᴀsᴇʏʀʜᴏᴅᴇs Tᴇᴄʜ`
         }, { quoted: mek });
     } catch (e) {
         console.log(e);
