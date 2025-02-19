51d6e1f8c0c2e263cdb309eb0275fca8e1464210
caseyweb <caseywebsthedonn25@gmail.com>
2025-02-19 12:52:40 +0300

    ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊🫅

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ console.log("Session downloaded ✅")
   console.log('Bot connected to whatsapp ✅')
   
   let up = `*Hello there ✦ CASEY ✦ RHODES ✦ XMD ✦ User! \ud83d\udc4b\ud83c\udffb* \n\n> This is auser friendly whatsapp bot created by Silva Tech Inc \ud83c\udf8a, Meet ✦ CASEYRHODES ✦ X ✦ MD ✦ WhatsApp Bot.\n\n *Thanks for using ✦ CASEY ✦ RHODES ✦ XMD ✦ \ud83d\udea9* \n\n> follow WhatsApp Channel :- 💖\n \nhttps://whatsapp.com/channel/0029VakUEfb4o7qVdkwPk83E\n\n- *YOUR PREFIX:* = ${prefix}\n\nDont forget to give star to repo ⬇️\n\nhttps://github.com/caseyweb/CASEYRHODES-XMD\n\n> © Powered BY ✦ CASEYRHODES ✦ XMD ✦ \ud83d\udda4`;
-  conn.sendMessage(conn.user.id, { image: { url: `https://i.imgur.com/PEZ5QL2.jpeg` }, caption: up })
+  conn.sendMessage(conn.user.id, { image: { url: `https://i.ibb.co/NdGZ99mN/705f0162-de6f-4fb8-a78f-6c563969093c.jpg` }, caption: up })
   }
   })
   conn.ev.on('creds.update', saveCreds)  
