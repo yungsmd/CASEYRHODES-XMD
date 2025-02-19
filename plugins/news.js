b15f1eaf7a80b1dd1d2f32672fefc4320eff5166
caseyweb <caseywebsthedonn25@gmail.com>
2025-02-19 15:18:05 +0300

    ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊🫅

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  ©⎈ Sɪʟᴠᴀ Ｓᴘᴀʀᴋ мᎠ ⎈
+  ©⎈ ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ⎈
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
