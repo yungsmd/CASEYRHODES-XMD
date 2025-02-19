07dc6ea176e259b39a85aa03ba0547ec8de155f2
caseyweb <caseywebsthedonn25@gmail.com>
2025-02-19 15:31:43 +0300

    ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊🫅

--- plugins/updater.js
+++ plugins/updater.js
@@ -11,7 +11,7 @@ cmd({
     filename: __filename,
 }, async (conn, mek, m, { from, reply }) => {
     try {
-        const repoUrl = 'https://github.com/SilvaTechB/silva-spark-md.git'; // GitHub repository URL
+        const repoUrl = 'https://github.com/caseyweb/CASEYRHODES-XMD.git'; // GitHub repository URL
         const targetFolder = 'plugins'; // Local folder for the repo
 
         // Ensure the target folder exists
@@ -38,7 +38,7 @@ cmd({
         // Send a success message with the output
         await conn.sendMessage(
             from,
-            { text: `*✅ Silva Spark Update completed successfully!*\n\n\`\`\`${output}\`\`\`` },
+            { text: `*✅ CASEYRHODES XMD Update completed successfully!*\n\n\`\`\`${output}\`\`\`` },
             { quoted: mek }
         );
     } catch (error) {
