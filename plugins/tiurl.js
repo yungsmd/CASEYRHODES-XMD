087589adfb6c8298e4675eb6404993eb0b8639fb
caseyweb <caseywebsthedonn25@gmail.com>
2025-02-20 14:06:16 +0300

    ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊🫅

---
+++ plugins/tiurl.js
@@ -0,0 +1,83 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const { fetchJson } = require('../lib/functions');
+
+
+cmd({
+  pattern: 'tinyurl',
+  alias: ['tiny', 'shorten', 'short', 'shorturl'],
+  react: '🪤',
+  desc: 'Shorten a URL using TinyURL or ShortURL.',
+  category: 'main',
+  filename: __filename
+}, async (conn, mek, m, {
+  from,
+  quoted,
+  body,
+  isCmd,
+  command,
+  args,
+  q,
+  isGroup,
+  sender,
+  senderNumber,
+  botNumber2,
+  botNumber,
+  pushname,
+  isMe,
+  isOwner,
+  groupMetadata,
+  groupName,
+  participants,
+  groupAdmins,
+  isBotAdmins,
+  isAdmins,
+  reply
+}) => {
+  try {
+    if (!q) return reply('Please provide a URL to shorten.');
+
+    await reply('> *Gmax Processing...*');
+
+    let apiUrl = '';
+    if (command === 'tiny' || command === 'tinyurl') {
+      apiUrl = `https://api.giftedtech.web.id/api/tools/tinyurl?apikey=gifted&url=${encodeURIComponent(q)}`;
+    } else {
+      apiUrl = `https://api.giftedtech.web.id/api/tools/shorturl?apikey=gifted&url=${encodeURIComponent(q)}`;
+    }
+
+    await reply('> *Shortening URL...*');
+
+    const response = await fetchJson(apiUrl);
+    const result = response.result;
+
+    const caption = ` \`CASEYRHODES XMD URL SHORTENER\` \n\n\n*Original Link:* ${q}\n\n*Shortened Link:* ${result}\n\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ caseyrhodes`;
+
+   /* await conn.sendMessage(m.chat, { text: caption }, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    reply(`An error occurred: ${error.message}`);
+  }
+});
+*/
+ // Send the status message with an image
+        await conn.sendMessage(from, { 
+            image: { url: `https://i.ibb.co/XkD69sBq/mrfrankofc.jpg` },  // Image URL
+            caption: caption,
+            contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363302677217436@newsletter',
+                    newsletterName: '『 𝐂𝐀𝐒𝐄𝐘𝐑𝐇𝐎𝐃𝐄𝐒 𝐗𝐌𝐃 』',
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: mek });
+
+    } catch (e) {
+        console.error("Error in shortining URL:", e);
+        reply(`An error occurred: ${e.message}`);
+    }
+});
