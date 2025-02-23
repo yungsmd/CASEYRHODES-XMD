51b8a74f756d6b619521e4202ee37547c32391f8
caseyweb <caseywebsthedonn25@gmail.com>
2025-02-23 16:18:40 +0300

    ℂ𝔸𝕊𝔼𝕐ℝℍ𝕆𝔻𝔼𝕊🫅

--- plugins/menu.js
+++ plugins/menu.js
@@ -64,7 +64,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://files.catbox.moe/dsrvw9.m4v' },
+            audio: { url: 'https://files.catbox.moe/vhj9ps.mp3' },
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
