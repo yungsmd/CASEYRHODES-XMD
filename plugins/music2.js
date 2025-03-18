2e12086e9da7b5e555724b9dbbbabbad35769506
caseyweb <caseywebsthedonn25@gmail.com>
2025-03-18 19:58:58 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ

--- plugins/music2.js
+++ plugins/music2.js
@@ -3,7 +3,7 @@ const yts = require("yt-search");
 const axios = require("axios");
 
 cmd({
-  pattern: "play",
+  pattern: "playx",
   alias: ["yt", "song", "video", "audio"],
   desc: "Download YouTube videos",
   category: "download",
