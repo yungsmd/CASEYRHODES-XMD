230ab79df1861822d4012acaaf15e6843df59b68
caseyweb <caseywebsthedonn25@gmail.com>
2025-03-19 15:16:00 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ

--- plugins/block.js
+++ plugins/block.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, isOwner, quoted, reply }) => {
     const user = quoted.sender;
     try {
         await conn.updateBlockStatus(user, 'block');
-        reply('🚫💎 Sɪʟᴠᴀ Sᴘᴀʀᴋ MD 💎 User ' + user + ' blocked successfully.');
+        reply('🚫 User ' + user + ' blocked successfully.');
     } catch (error) {
         reply('❌ Error blocking user: ' + error.message);
     }
